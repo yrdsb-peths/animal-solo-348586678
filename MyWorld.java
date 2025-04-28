f94c25d16fe9326ca3474b232d02b257b06ab650
348586678 <348586678@gapps.yrdsb.ca>
2025-04-28 13:49:10 -0400

    Added Frog object

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Frog frog = new Frog();
+        addObject(frog, 100, 200);
     }
 }
